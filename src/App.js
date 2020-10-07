f891bbc256b18b70c76f11f4413ebb840497b0e7
Marcelo Del Valle <cheloxnz@gmail.com>
2020-10-07 18:43:34 -0300

    redux conectado

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import Home from './pages/Home';
+import Results from './pages/results/index';
+import MovieDetail from './pages/MovieDetail/index';
 
 const App = ({ store }) => (
-  // <Provider store={store}>
-  <Router>
-    <div>
-      <Route exact path='/' component={Home} />
-    </div>
-  </Router>
-  // </Provider>
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/results' component={Results} />
+        <Route exact path='/movie/:id' component={MovieDetail} />
+      </div>
+    </Router>
+  </Provider>
 );
 
 App.propTypes = {
